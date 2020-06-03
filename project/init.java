32c9f2a68e765bb932dc7f65302c6326e82296d7
jin20415 <63662168+jin20415@users.noreply.github.com>
2020-06-04 00:59:11 +0900

    Update init.java

--- project/init.java
+++ project/init.java
@@ -3,10 +3,10 @@ package project;
 import javax.swing.JOptionPane;
 
 class init {
-	static int DdayNumber = 45;
+	static int DdayNumber = 14;
 	static int Power = 100;
-	static int Money = 100000;
-	static int Friend = 50;
+	static int Money = 50000;
+	static int Friend = 0;
 	static int Intelli = 0;
 	static int Month = 5;
 	static int Day = 1;
@@ -36,20 +36,23 @@ class init {
 	}
 
 	static void 오픈소스이해() {
-		오픈소스이해도 +=5;
-		JOptionPane.showMessageDialog(null, "오픈 소스 소프트웨어 과목의 이해도가 5 증가했습니다!", "이해도",JOptionPane.INFORMATION_MESSAGE);
+		오픈소스이해도 += 5;
+		DdayNumber--;
+		JOptionPane.showMessageDialog(null, "오픈 소스 소프트웨어 과목의 이해도가 5 증가했습니다!", "이해도", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	static void 데이터베이스이해() {
-		데이터베이스이해도 +=5;
-		JOptionPane.showMessageDialog(null, "데이터베이스 과목의 이해도가 5 증가했습니다!", "이해도",JOptionPane.INFORMATION_MESSAGE);
+		데이터베이스이해도 += 5;
+		DdayNumber--;
+		JOptionPane.showMessageDialog(null, "데이터베이스 과목의 이해도가 5 증가했습니다!", "이해도", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	static void 정보보호이해() {
-		정보보호이해도 +=5;
-		JOptionPane.showMessageDialog(null, "정보보호 과목의 이해도가 5 증가했습니다!", "이해도",JOptionPane.INFORMATION_MESSAGE);
+		정보보호이해도 += 5;
+		DdayNumber--;
+		JOptionPane.showMessageDialog(null, "정보보호 과목의 이해도가 5 증가했습니다!", "이해도", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	static void N_Lecture_power() {
 		int random = (int) (Math.random() * 20 + 1);
 		if (random >= 17) {
@@ -69,13 +72,39 @@ class init {
 		Money -= 10000;
 		MT();
 	}
+	
+	static int newday() {
+		DdayNumber= DdayNumber-1;
+		Day++;
+		return DdayNumber;
+		
+	}
+
+	static void H_Lecture_power() {
+		int random = (int) (Math.random() * 20 + 1);
+		if (random >= 17) {
+			Intelli += 20;
+			JOptionPane.showMessageDialog(null, "너무 열심히 했다.\n 지식이 20만큼 증가했다!", "너무 힘듦",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else if (random >= 8 && random < 17) {
+			Intelli += 10;
+			JOptionPane.showMessageDialog(null, "오늘은 적당히 했다.\n 지식이 10만큼 증가했다!", "조금 기쁘네",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			Intelli += 5;
+			JOptionPane.showMessageDialog(null, "대충대충 했다.\n 지식이 5만큼 증가했다!", "넘나 귀찮은 것",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+		Power -= 30;
+		Money -= 10000;
+		MT();
+	}
+
 	static void N_intelli_up() {
-		
-		
+
 		Day += 1;
 		DdayNumber -= 1;
-		
-		
+
 	}
 
 	static void N_Study() {
@@ -101,21 +130,20 @@ class init {
 
 	static void N_money_up() {
 		int random = (int) (Math.random() * 20 + 1);
-		if (random >= 17) {
-			Money += 40000;
-			JOptionPane.showMessageDialog(null, "보너스를 받았다!\n 알바비를 4만원 받았다!", "너무 힘듦", JOptionPane.INFORMATION_MESSAGE);
-		} else if (random >= 8 && random < 17) {
-			Money += 30000;
-			JOptionPane.showMessageDialog(null, "오늘은 적당히 했다.\n 알바비를 3만원 받았다!", "조금 기쁘네",
+		if (random >= 10) {
+			Money += 100000;
+			JOptionPane.showMessageDialog(null, "보너스를 받았다!\n 알바비를 10만원 받았다!", "너무 힘듦", JOptionPane.INFORMATION_MESSAGE);
+		} else if (random >= 5) {
+			Money += 50000;
+			JOptionPane.showMessageDialog(null, "오늘은 적당히 했다.\n 알바비를 5만원 받았다!", "조금 기쁘네",
 					JOptionPane.INFORMATION_MESSAGE);
 		} else {
-			Money += 20000;
-			JOptionPane.showMessageDialog(null, "의욕이 넘쳐 실수를 했다.\n 알바비를 2만원 받았다!", "앗 안돼!",
+			Money += 30000;
+			JOptionPane.showMessageDialog(null, "의욕이 넘쳐 실수를 했다.\n 알바비를 3만원 받았다!", "앗 안돼!",
 					JOptionPane.INFORMATION_MESSAGE);
 		}
 		Power -= 25;
-		Day += 1;
-		DdayNumber -= 1;
+		
 		MT();
 	}
 
@@ -128,38 +156,84 @@ class init {
 		MT();
 	}
 
+	static void N_sing() {
+		Money -= 20000;
+		JOptionPane.showMessageDialog(null, "노래를 부르는데 2만원을 썼어!", "노래방",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	static void N_cinema() {
+		Money -= 30000;
+		JOptionPane.showMessageDialog(null, "티켓과 팝콘을 사느라 3만원을 썼어!", "영화관",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	static void N_park() {
+		Money -= 50000;
+		JOptionPane.showMessageDialog(null, "입장권과 밥을 먹는데 5만원을 썼어!", "놀이공원",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	static void H_sing() {
+		Money -= 25000;
+		JOptionPane.showMessageDialog(null, "노래를 부르는데 2만5천원을 썼어!", "노래방",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	static void H_cinema() {
+		Money -= 35000;
+		JOptionPane.showMessageDialog(null, "티켓과 팝콘을 사느라 3만5천원을 썼어!", "영화관",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	static void H_park() {
+		Money -= 55000;
+		JOptionPane.showMessageDialog(null, "티켓과 팝콘을 사느라 5만5천원을 썼어!", "영화관",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
 	static void N_rest() {
 		int random = (int) (Math.random() * 20 + 1);
 		if (random >= 17) {
+			Power += 25;
 			Friend += 30;
 			JOptionPane.showMessageDialog(null, "베프가 되었다!\n 친화력이 30만큼 증가했다!", "너무 즐거움",
 					JOptionPane.INFORMATION_MESSAGE);
 		} else if (random >= 8 && random < 17) {
+			Power += 10;
 			Friend += 20;
 			JOptionPane.showMessageDialog(null, "재미있게 놀았다!.\n 친화력이 20만큼 증가했다!", "즐거움", JOptionPane.INFORMATION_MESSAGE);
 		} else {
+			Power += 5;
 			Friend += 10;
 			JOptionPane.showMessageDialog(null, "조금 어색했네...\n 친화력이 10만큼 증가했다!", "어색해", JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
-	/*
-	 * static void N_power_up() { int random = (int) (Math.random() * 20 + 1); if
-	 * (random >= 17) { Power += 25; JOptionPane.showMessageDialog(null,
-	 * "이런 휴식이 올까?\n 체력이이 25만큼 올랐다!", "컨디션 좋아", JOptionPane.INFORMATION_MESSAGE); }
-	 * else if (random >= 8 && random < 17) { Power += 15;
-	 * JOptionPane.showMessageDialog(null, "너무 잘 쉬었다.\n 체력이 15만큼 올랐다!", "즐거움",
-	 * JOptionPane.INFORMATION_MESSAGE); } else { Power += 5;
-	 * JOptionPane.showMessageDialog(null, "자고 일어났는데 담이 걸렸네...\n 체력이 5만큼 올랐다!",
-	 * "어색해", JOptionPane.INFORMATION_MESSAGE); } Day += 1; DdayNumber -= 1; MT(); }
-	 */
-
+	static void H_rest() {
+		int random = (int) (Math.random() * 20 + 1);
+		if (random >= 10) {
+			Power += 30;
+			Friend += 20;
+			JOptionPane.showMessageDialog(null, "베프가 되었다!\n 친화력이 20만큼 증가했다!", "너무 즐거움",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else if (random >= 5) {
+			Power += 20;
+			Friend += 10;
+			JOptionPane.showMessageDialog(null, "재미있게 놀았다!.\n 친화력이 10만큼 증가했다!", "즐거움", JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			Power += 5;
+			Friend += 5;
+			JOptionPane.showMessageDialog(null, "조금 어색했네...\n 친화력이 5만큼 증가했다!", "어색해", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+	
 	static void N_Cold() {
 		for (int count = 0; count < 1; count++) {
 			if (Power <= 30) {
 				Day += 1;
 				DdayNumber -= 1;
 				MT();
-				Power += 30;
+				Power += 20;
 				count++;
 				JOptionPane.showMessageDialog(null, "체력이 약해져서 감기에 걸렸다!\n오늘 하루동안 집에서 쉬어야겠다", "감기",
 						JOptionPane.WARNING_MESSAGE);
@@ -215,14 +289,14 @@ class init {
 	static void H_money_up() {
 		int random = (int) (Math.random() * 20 + 1);
 		if (random >= 15) {
-			Money += 40000;
+			Money += 130000;
 			JOptionPane.showMessageDialog(null, "너무 열심히 했다.\n 알바비를 4만원 받았다!", "너무 힘듦", JOptionPane.INFORMATION_MESSAGE);
 		} else if (random >= 13 && random < 17) {
-			Money += 20000;
+			Money += 80000;
 			JOptionPane.showMessageDialog(null, "오늘은 적당히 했다.\n 알바비를 2만원 받았다!", "조금 기쁘네",
 					JOptionPane.INFORMATION_MESSAGE);
 		} else {
-			Money += 10000;
+			Money += 50000;
 			Power -= 5;
 			JOptionPane.showMessageDialog(null, "의욕이 넘쳐 실수를 했다.\n 체력이 5만큼 감소하고 알바비를 1만원 받았다!", "앗 안돼!",
 					JOptionPane.INFORMATION_MESSAGE);
@@ -233,38 +307,7 @@ class init {
 		MT();
 	}
 
-	static void H_friend_up() {
-		int random = (int) (Math.random() * 20 + 1);
-		if (random >= 17) {
-			Friend += 20;
-			JOptionPane.showMessageDialog(null, "베프가 되었다!\n 친화력이 20만큼 증가했다!", "너무 즐거움",
-					JOptionPane.INFORMATION_MESSAGE);
-		} else if (random >= 13 && random < 17) {
-			Friend += 10;
-			JOptionPane.showMessageDialog(null, "재미있게 놀았다!.\n 친화력이 10만큼 증가했다!", "즐거움", JOptionPane.INFORMATION_MESSAGE);
-		} else {
-			Friend += 5;
-			Power -= 5;
-			JOptionPane.showMessageDialog(null, "어색했네... 눈치봐서 체력이 5만큼 감소했다!\n 친화력이 5만큼 증가했다!", "어색해",
-					JOptionPane.INFORMATION_MESSAGE);
-		}
-		Money -= 30000;
-		Power -= 20;
-		Day += 1;
-		DdayNumber -= 1;
-		MT();
-	}
 
-	/*
-	 * static void H_power_up() { int random = (int) (Math.random() * 20 + 1); if
-	 * (random >= 17) { Power += 25; JOptionPane.showMessageDialog(null,
-	 * "이런 휴식이 올까?\n 체력이이 25만큼 올랐다!", "컨디션 좋아", JOptionPane.INFORMATION_MESSAGE); }
-	 * else if (random >= 8 && random < 17) { Power += 15;
-	 * JOptionPane.showMessageDialog(null, "너무 잘 쉬었다.\n 체력이 15만큼 올랐다!", "즐거움",
-	 * JOptionPane.INFORMATION_MESSAGE); } else { Power += 5;
-	 * JOptionPane.showMessageDialog(null, "자고 일어났는데 담이 걸렸네...\n 체력이 5만큼 올랐다!",
-	 * "어색해", JOptionPane.INFORMATION_MESSAGE); } Day += 1; DdayNumber -= 1; MT(); }
-	 */
 
 	static void H_Cold() {
 		int random = (int) (Math.random() * 10 + 1);
